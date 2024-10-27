ad109c8a15901c07bbea9dfed491c899a1a4f3d0
flyzet-prog <73076482+flyzet-prog@users.noreply.github.com>
2024-10-27 13:25:39 +0200

    Add files via upload

---
+++ firebase_poster.cpp
@@ -0,0 +1,21 @@
+#include "esphome.h"
+#include <HTTPClient.h>
+
+class FirebasePoster : public Component {
+  public:
+    void send_data_to_firebase(float state_of_charge) {
+      HTTPClient http;
+      http.begin("https://esp32-2ed9f-default-rtdb.firebaseio.com/your_path.json?auth=v2HEeXenilhVZsinz7xgVe3bfN72");  // Firebase URL
+      http.addHeader("Content-Type", "application/json");
+
+      String payload = "{\"state_of_charge\": " + String(state_of_charge) + "}";
+      
+      int httpResponseCode = http.POST(payload);
+      if (httpResponseCode > 0) {
+        ESP_LOGD("custom", "Data sent successfully: %d", httpResponseCode);
+      } else {
+        ESP_LOGE("custom", "Error sending data: %d", httpResponseCode);
+      }
+      http.end();  // Free resources
+    }
+};
